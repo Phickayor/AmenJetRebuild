32d2ad60c6e7c6d31b6df0cb143e096691e691a3
Fikayo Jetawo <jetawof@gmail.com>
2022-11-29 19:34:59 +0100

    working on NavBar component

--- amen-jet.html/src/components/NavBar.js
+++ amen-jet.html/src/components/NavBar.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div class="sticky h-30 border bg-none">
+      <h1 className="absolute p-5 text-3xl font-serif text-blue-500">
+        Amen Jet
+      </h1>
+      <ul className=" hidden inline text-lg lg:flex font-mono justify-center mt-0 space-x-10 p-5">
+        <li>Home</li>
+        <li>Services</li>
+        <li>Gallery</li>
+        <li>Reviews</li>
+        <li>Goals</li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;

a252c0a2dace9dea0781c86cc816a29d9035d487
Fikayo Jetawo <jetawof@gmail.com>
2022-12-01 01:32:55 +0100

    working on review component

---
+++ amen-jet.html/src/App.js
@@ -0,0 +1,29 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Gallery from "./components/Gallery";
+import Reviews from "./components/Reviews";
+import Goals from "./components/Goals";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <NavBar />
+        <Home />
+      </div>
+      <Services />
+      <Goals />
+      <Gallery />
+      <Reviews />
+      {/* <Footer />
+      <Contact />
+      */}
+    </div>
+  );
+}
+
+export default App;
--- amen-jet.html/src/components/Reviews.js
+++ amen-jet.html/src/components/Reviews.js
@@ -1,6 +1,114 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 function Reviews() {
-  return <div>Reviews</div>;
+  const [slideNumber, setSlideNumber] = useState(1);
+
+  function slidesNumber() {
+    if (window.innerWidth >= 1024) {
+      setSlideNumber(3);
+    } else if (window.innerWidth >= 600) {
+      setSlideNumber(2);
+    } else {
+      setSlideNumber(1);
+    }
+  }
+
+  // create an event listener
+  useEffect(() => {
+    window.addEventListener("load", slidesNumber);
+    window.addEventListener("resize", slidesNumber);
+  });
+  const reviews = [
+    {
+      service: "Mobile Nursing",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    },
+    {
+      service: "Home Embambling",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    },
+    {
+      service: "Mental Nursing",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    },
+    {
+      service: "Mobile Nursing",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    },
+    {
+      service: "Home Embambling",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    },
+    {
+      service: "Mental Nursing",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    },
+    {
+      service: "Mobile Nursing",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    },
+    {
+      service: "Home Embambling",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    },
+    {
+      service: "Mental Nursing",
+      review: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Fikayo Jetawo"
+    }
+  ];
+  return (
+    <>
+      <h1 className="text-4xl font-mono text-blue-500 mt-10">Reviews</h1>
+      <p className="text-lg font-mono mb-10">
+        What people say about our services.
+      </p>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={slideNumber}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        {reviews.map((review, index) => (
+          <SwiperSlide
+            key={index}
+            className="border-blue-500 h-32 min-h-max p-10 border-2 text-center rounded-full shadow-2xl"
+          >
+            <h1 className="text-3xl border-b p-5 border-white rounded-3xl font-serif">
+              {review.service}
+            </h1>
+            <q className="text-lg font-mono w-full">{review.review}</q>
+            <br />
+            <cite className="block m-2">{"~" + review.author}</cite>
+          </SwiperSlide>
+        ))}
+        <h1 className="mt-5">...</h1>
+      </Swiper>
+    </>
+  );
 }
 export default Reviews;

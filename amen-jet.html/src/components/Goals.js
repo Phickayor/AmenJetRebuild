21bac04d47d684e378290cf6ebad0823b48cd8b6
Fikayo Jetawo <jetawof@gmail.com>
2022-12-01 01:32:17 +0100

    reducing border radius on smaller devices

--- amen-jet.html/src/components/Goals.js
+++ amen-jet.html/src/components/Goals.js
@@ -6,7 +6,7 @@ function Goals() {
       <h1 className="text-4xl font-mono text-blue-500">Goals</h1>
       <p className="text-lg font-mono mb-10">The reason we do what we do </p>
       <div className="flex justify-center">
-        <div className="border-4 border-blue-500 w-4/5 rounded-full">
+        <div className="border-4 border-blue-500 rounded-3xl w-4/5 lg:rounded-full">
           <div className="p-10">
             <h1 className="font-serif text-3xl text-blue-500">Our Mission</h1>
             <p className="font-mono text-lg mt-2">

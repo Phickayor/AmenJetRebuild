d81b22f4e4925f5487fd5052ad3da38f390a812f
Fikayo Jetawo <jetawof@gmail.com>
2022-12-01 01:30:44 +0100

    class ->  className where mistakes were made

--- amen-jet.html/src/components/Home.js
+++ amen-jet.html/src/components/Home.js
@@ -11,7 +11,7 @@ function Home() {
         />
       </div>
       <div className="md:w-1/2 w-full p-10 md:order-1 flex justify-center flex-col border">
-        <h1 class="font-serif text-6xl ">AMEN JET MEDICAL SERVICES</h1>
+        <h1 className="font-serif text-6xl ">AMEN JET MEDICAL SERVICES</h1>
         <p className="font-mono p-5 text-lg ">
           Bringing quality health care to your door step.
         </p>
@@ -20,13 +20,13 @@ function Home() {
         </button>
       </div>
       <svg
-        className="order-3 w-full"
+        className="order-3"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
       >
         <path
           fill="#0099ff"
-          fill-opacity="1"
+          fillOpacity="1"
           d="M0,96L120,101.3C240,107,480,117,720,149.3C960,181,1200,235,1320,261.3L1440,288L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
         ></path>
       </svg>

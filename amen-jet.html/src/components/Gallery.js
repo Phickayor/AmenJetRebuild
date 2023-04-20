35c2be9467e82338dfca0c3dc65a3913412531f4
Fikayo Jetawo <jetawof@gmail.com>
2023-04-20 23:24:47 +0100

    Gallery component for desktop

--- amen-jet.html/src/components/Gallery.js
+++ amen-jet.html/src/components/Gallery.js
@@ -2,37 +2,62 @@ import React from "react";
 
 function Gallery() {
   return (
-    <div id="gallery" className="mt-20 ">
-      {/* <h1 className="text-4xl font-mono text-blue-500 mt-10">Gallery</h1>
-      <p className="text-lg font-mono mb-10">
-        Relive the moments with us. Memories lasts forever.
-      </p> */}
-      <div
-        style={{ height: "35rem" }}
-        className="mx-auto my-0 w-full md:w-4/5  bg-gray-900 flex justify-center rounded-3xl"
-      >
-        <img
-          style={{ height: "35rem" }}
-          className="w-full rounded-3xl opacity-40"
-          src="https://images.pexels.com/photos/6129207/pexels-photo-6129207.jpeg?auto=compress&cs=tinysrgb&w=2000"
-          alt="Gallery-Background"
-        />
-        <div
-          style={{ height: "35rem" }}
-          className="absolute flex justify-center text-white flex-col w-full "
-        >
-          <h1 className="text-5xl m-2 text-blue-200">Amen Jet Gallery</h1>
-          <p className="text-lg font-mono mb-5">
-            Take a journey with us to visit our memories one after the other.
-          </p>
-          <a href="https://amenjet.pixieset.com/amenjetgallery">
-            <button className=" font-semibold p-5 rounded-3xl bg-blue-500 w-1/2 hover:w-full">
-              View Gallery
-            </button>
-          </a>
+    <div className="w-10/12 mx-auto py-10">
+      <div className='font-rubik text-headingfont px-10 my-5'>
+        <p className='bg-secondary1 inline px-2 py-1 border-l-4 border-primary1'>Our Gallery</p>
+        <h1 className='text-4xl font-semibold py-4'>With every <b className="text-blue-500 font-semibold">Click</b>, a <b className="font-semibold text-blue-500">Story</b> to tell</h1>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div className="grid gap-2">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/hero.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/home.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image2.jpg" alt="" />
+          </div>
+        </div>
+        <div className="grid gap-2">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image3.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image4.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image5.jpg" alt="" />
+          </div>
+        </div>
+        <div className="grid gap-2">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image6.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image7.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image8.jpg" alt="" />
+          </div>
+        </div>
+        <div className="grid gap-2">
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image9.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image1.jpg" alt="" />
+          </div>
+          <div>
+            <img className="h-auto max-w-full rounded-lg" src="/assets/Amen Jet/image10.jpg" alt="" />
+          </div>
         </div>
       </div>
+      <a href="https://amenjet.pixieset.com/amenjetgallery/">
+        <button className="btns my-5">Show More</button>
+      </a>
     </div>
+
   );
 }
 

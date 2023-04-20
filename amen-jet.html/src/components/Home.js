907172e582ba61b819cab112e31e59755d74e0e6
Fikayo Jetawo <jetawof@gmail.com>
2023-04-20 23:21:13 +0100

    New home component for desktop

--- amen-jet.html/src/components/Home.js
+++ amen-jet.html/src/components/Home.js
@@ -1,37 +1,34 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react"
 function Home() {
-  return (
-    <div id="home" className="flex flex-wrap flex-grow top-0">
-      <div className="bg-gray-100 md:order-2 max-h-screen overflow-hidden md:w-1/2 border">
-        <img
-          className="w-full h-full md:h-fit "
-          src="https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=2000"
-          alt="doctor"
-        />
-      </div>
-      <div className="md:w-1/2 w-full p-10 md:order-1 flex justify-center flex-col border">
-        <h1 className="font-serif text-6xl ">AMEN JET MEDICAL SERVICES</h1>
-        <p className="font-mono p-5 text-lg ">
-          Bringing quality health care to your door step.
-        </p>
-        <button className="font-semibold border p-10 text-white text-xl w-80 self-center bg-blue-500 rounded-3xl">
-          Book Appointment
-        </button>
-      </div>
-      <svg
-        className="order-3 w-full"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-      >
-        <path
-          fill="#0099ff"
-          fillOpacity="1"
-          d="M0,96L120,101.3C240,107,480,117,720,149.3C960,181,1200,235,1320,261.3L1440,288L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
-        ></path>
-      </svg>
-    </div>
-  );
-}
+  var backgrounds = ["/assets/home.jpg", "/assets/hero.jpg", "/assets/Amen Jet/image1.jpg", "/assets/Amen Jet/image7.jpg", "/assets/Amen Jet/image3.jpg"]
+  var quotes = ["The greatest wealth is health", "Invest in your health today or pay for your illness tomorrow", "Good health and good sense are two of life's greatest blessings",
+    "Take care of your body. It's the only place you have to live"]
+  var [currentCount, setcurrentCount] = useState(0)
+  function counter() {
+    if (currentCount < 4) {
+      setcurrentCount(currentCount++)
+    }
+    else {
+      setcurrentCount(0)
+    }
+  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      counter()
+    }, 10000);
+    return () => clearInterval(interval)
+  })
 
-export default Home;
+
+  return (
+    <div className="relative bg-slate-900">
+      <img src={backgrounds[currentCount]} alt="" className="w-full left-0 absolute h-full object-cover opacity-50" />
+      <div className="z-20 relative flex flex-col justify-center w-10/12 mx-auto p-20 space-y-5 text-white h-[550px] ">
+        <p className='bg-[#041C3780]  self-center block px-2 py-1 border-l-4 border-primary1'>Amen Jet Medical Services</p>
+        <h5 className="font-rubik text-7xl font-bold leading-[71.1px]">{quotes[currentCount]} </h5>
+        <button className="btns text-primary2 font-semibold bg-gradient-to-r from-[#FFB629] via-[#FFDA56] to-[#FFD7A6] after:bg-white">Explore More</button>
+      </div>
+    </div>
+  )
+}
+export default Home

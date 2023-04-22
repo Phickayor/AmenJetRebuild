cebeaf6067982030652397b9d21d7f947bb4c2cf
Fikayo Jetawo <jetawof@gmail.com>
2023-04-22 13:03:29 +0100

    removing all warnings

--- amen-jet.html/src/App.js
+++ amen-jet.html/src/App.js
@@ -7,21 +7,18 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "./App.css";
-import Faqs from "./components/Faqs";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <About />
       <Services />
       <Gallery />
       <Reviews />
       <Contact />
       <Footer />
-      {/*
-      <About />
-      <Faqs /> */}
     </div >
   );
 }

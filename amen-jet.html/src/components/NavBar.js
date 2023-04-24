fca81c1ad827e829deb8ccd8c7403374ab44407b
Fikayo Jetawo <jetawof@gmail.com>
2023-04-24 16:24:54 +0100

    navbar finishing touches and change of about image

--- amen-jet.html/src/components/NavBar.js
+++ amen-jet.html/src/components/NavBar.js
@@ -25,13 +25,11 @@ function NavBar() {
     }
   }
   return (
-    <div
-      className="sticky top-0 left-0  border-b flex z-30 justify-between backdrop-blur md:px-20 bg-white"
-    >
-      <h1 className=" text-4xl font-rubik p-5 text-blue-500 font-semibold">
+    <div className="sticky top-0 left-0  border-b flex z-30 justify-between backdrop-blur px-5 md:px-5">
+      <h1 className=" text-4xl font-rubik p-2 self-center text-blue-500 font-semibold">
         Amen Jet
       </h1>
-      <ul className="hidden lg:flex font-krub  text-2xl justify-center mt-0 space-x-10 p-5">
+      <ul className="hidden lg:flex font-krub text-2xl justify-center mt-0 space-x-10 p-3">
         <a href="/">
           <li className="hover:bg-primary2 p-2 hover:text-white rounded-lg">
             Home
@@ -63,9 +61,9 @@ function NavBar() {
           </li>
         </a>
       </ul>
-      <div>
+      <div className="lg:hidden ">
         <div
-          className="md:hidden self-center cursor-pointer"
+          className=" self-center cursor-pointer"
           onClick={() => toggleSideBar()}
         >
           {harmburger}
@@ -74,20 +72,19 @@ function NavBar() {
           className="absolute duration-[1.5s] right-0 top-0 l h-screen fixed overflow-y-auto bg-primary2 text-white flex-col flex"
           style={{ width: sideBarWidth }}
         >
-          <div className=" my-5 ">
-            <div className="flex justify-between sticky top-4 backdrop-blur">
+          <div className=" py-5 ">
+            <div className="flex justify-between sticky top-0 backdrop-blur">
               <div className='flex space-x-2 ml-2 my-2 self-center'>
                 <img src='/favicon.ico' alt="" className="sm:block hidden h-20" />
                 <h1 className='text-3xl md:text-4xl font-rubik self-center font-semibold'>Amen Jet</h1>
               </div>
               <button
-                className="mr-5 self-center bg-white text-primary2 rounded-full w-16 h-16"
+                className="mt-2 mr-5 self-center bg-white text-primary2 rounded-full w-16 h-16"
                 onClick={() => toggleSideBar()}
               >
                 {close}
               </button>
             </div>
-
             <div className="flex flex-col text-left mt-10 mx-auto w-11/12 font-light text-2xl space-y-10">
               <a href="#home" onClick={() => toggleSideBar()} className="flex space-x-4 px-5 rounded-xl py-4 hover:bg-gray-900 px-5 rounded-xl py-4">
                 <FontAwesomeIcon icon={faHome} className="self-center" />
